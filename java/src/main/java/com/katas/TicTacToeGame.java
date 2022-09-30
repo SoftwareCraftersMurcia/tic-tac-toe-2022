5bb805e1acc895726138a83f2202b2b172e7cb4a
CosmeValera <cosmevalerareales@gmail.com>
2022-09-30 18:56:10 +0200

    check first move is O

--- java/src/main/java/com/katas/TicTacToeGame.java
+++ java/src/main/java/com/katas/TicTacToeGame.java
@@ -3,6 +3,8 @@ package com.katas;
 public class TicTacToeGame {
 
   public void play(String player, int row, int column) {
-    throw new XUserShouldGoFirstException();
+    if (player.equals("O")) {
+      throw new XUserShouldGoFirstException();
+    }
   }
 }
--- java/src/test/java/com/katas/TicTacToeGameTest.java
+++ java/src/test/java/com/katas/TicTacToeGameTest.java
@@ -6,14 +6,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TicTacToeGameTest {
 
-	@Test
-	public void should_throw_exception_when_O_user_plays_first() {
+  @Test
+  public void should_throw_exception_when_O_user_plays_first() {
     assertThatThrownBy(() -> {
       TicTacToeGame ticTacToeGame = new TicTacToeGame();
 
       ticTacToeGame.play("O", 0, 0);
     })
-    .isInstanceOf(XUserShouldGoFirstException.class);
-	}
+      .isInstanceOf(XUserShouldGoFirstException.class);
+  }
+
+  @Test
+  public void should_not_throw_exception_when_X_user_plays_first() {
+    TicTacToeGame ticTacToeGame = new TicTacToeGame();
+
+    ticTacToeGame.play("X", 0, 0);
+  }
+
+
 
 }
