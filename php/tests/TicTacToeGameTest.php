b20273e1f18590669994542d5928beab88881584
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-09-30 18:58:18 +0200

    refactor into dataProvider

--- php/tests/TicTacToeGameTest.php
+++ php/tests/TicTacToeGameTest.php
@@ -13,30 +13,33 @@ final class TicTacToeGameTest extends TestCase
     /**
      * - [x] X always goes first
      * - [X] A Player can not play twice
-     * - [ ] Players alternate placing X’s and O’s on the board
+     * - [X] Players alternate placing X’s and O’s on the board
      * - [ ] Players cannot play on a played position
      * - [ ] A player with 3 X’s (vertically, horizontally or diagonally) wins the game.
      * - [ ] If all 9 squares are filled and neither player achieves 3 in a row, the game is a draw.
      * - [ ] A player with 3 O’s in a row (vertically, horizontally or diagonally) wins the game.
      */
 
-    /** @test */
-    public function assert_x_always_goes_first(): void
+    public function playerDataProvider(): iterable
     {
-        $this->expectException(RuntimeException::class);
-        $this->expectExceptionMessage('Invalid current Player');
-
-        (new TicTacToeGame())->play('O');
+        yield "assert_x_always_goes_first" => [['O']];
+        yield "assert_a_player_cant_play_twice" => [['X', 'X']];
+        yield "assert_a_player_should_play_alternative" => [['X', 'O', 'O']];
     }
 
-    /** @test */
-    public function assert_a_player_cant_play_twice(): void
+    /**
+     * @test
+     * @dataProvider playerDataProvider()
+     */
+    public function XX(array $players): void
     {
         $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Invalid current Player');
 
         $ticTacToeGame = new TicTacToeGame();
-        $ticTacToeGame->play('X');
-        $ticTacToeGame->play('X');
+
+        foreach ($players as $player) {
+            $ticTacToeGame->play($player);
+        }
     }
 }
